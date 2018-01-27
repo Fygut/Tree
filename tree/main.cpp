4ff01aef2f8e188874f2475203846e5111dbc558
Stanislav Romanov <Fygut@ya.ru>
2018-01-28 04:01:50 +0700

    Read input and output file name from the console

--- tree/main.cpp
+++ tree/main.cpp
@@ -4,7 +4,7 @@ int main(int argc, char **argv)
 { 
 	try
 	{
-		/*if (argc != 5)throw "Input parametr error";
+		if (argc != 5)throw "Input parametr error";
 		if (argv[1][1] == 'i' && argv[3][1] == 'o')	{
 			system("cls");
 			Tree a(argv[2]);
@@ -18,12 +18,12 @@ int main(int argc, char **argv)
 				a.out_tree_on_console(a.get_root(), 0, vector< size_t >(0));
 				a.output(argv[2]);
 			}
-			else throw "Input parametr error";*/
-			string input = "input.txt";
+			else throw "Input parametr error";
+			/*string input = "input.txt";
 			string output = "output.txt";
 			Tree a(input);
 			a.out_tree_on_console(a.get_root(), 0, vector< size_t >(0));
-			a.output(output);
+			a.output(output);*/
 	}
 	catch (const char e[])
 	{
